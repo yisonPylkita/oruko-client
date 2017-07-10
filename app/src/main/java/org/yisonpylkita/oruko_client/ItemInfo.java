0487f47fc25f79c42999bb47dbb8705dede9d2b5
Wojciech Bartnik <yisonPylkita@gmail.com>
2017-07-10 08:24:21 +0200

    ItemInfo will show item informations

---
+++ app/src/main/java/org/yisonpylkita/oruko_client/ItemInfo.java
@@ -0,0 +1,13 @@
+package org.yisonpylkita.oruko_client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItemInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_info);
+    }
+}
