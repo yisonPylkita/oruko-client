d6702f31bc037eb7530a846046accceb4aaba626
Wojciech Bartnik <yisonPylkita@gmail.com>
2017-07-12 17:36:28 +0200

    Barcode scanning implemented

--- app/src/main/java/org/yisonpylkita/oruko_client/MainActivity.java
+++ app/src/main/java/org/yisonpylkita/oruko_client/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,14 +23,29 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ItemInfo.class);
+                Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                intent.putExtra("SCAN_MODE", "BAR_CODE_MODE");
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent barcodeIntent) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                String contents = barcodeIntent.getStringExtra("SCAN_RESULT");
+                String format = barcodeIntent.getStringExtra("SCAN_RESULT_FORMAT");
+
+                Intent itemIntent = new Intent(MainActivity.this, ItemInfo.class);
                 ItemInfoModel model = new ItemInfoModel();
                 model.name = "Foo";
                 model.id = "123";
                 model.description = "Some text";
-                intent.putExtra(ITEM_INFO_JSON_NAME, model.toString());
-                startActivity(intent);
+                itemIntent.putExtra(ITEM_INFO_JSON_NAME, model.toString());
+                startActivity(itemIntent);
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(getApplicationContext(), "Ups.. Try again", Toast.LENGTH_SHORT);
             }
-        });
+        }
     }
 }
